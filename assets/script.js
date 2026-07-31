b28d74a8499b7632b0ae658ed57b4fe22e5f8e49
seriin05 <asereen53@gmail.com>
2026-08-01 01:32:03 +0300

    Add files via upload

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-var menuBtn = document.getElementById("menu-btn");
-var navMenu = document.querySelector(".nav-links");
-
-menuBtn.addEventListener("click", function () {
-    navMenu.classList.toggle("active");
-});
